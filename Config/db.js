3d1025a4381a3b78807d75d0f14a2689e9c3c89c
“Abdul <kaazees31@gmail.com>
2024-09-19 20:49:26 +0530

    UPDATE

--- Config/db.js
+++ Config/db.js
@@ -2,13 +2,10 @@ import mongoose from "mongoose";
 
 const db = async () => {
   try {
-    const con = await mongoose.connect(process.env.MONGO_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const con = await mongoose.connect(process.env.MONGO_URL);
     console.log(`MongoDB connected: ${con.connection.host}`);
   } catch (error) {
-    console.error(`Error: ${error.message}`); // Log the actual error message
+    console.error(`Error: ${error.message}`);
     process.exit(1); // Exit the process if there's an error
   }
 };
