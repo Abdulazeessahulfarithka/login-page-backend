cfecb04daa1af0e63afb1ec363c02898def297b7
“Abdul <kaazees31@gmail.com>
2024-09-19 12:11:18 +0530

    u

--- index.js
+++ index.js
@@ -1,36 +1,43 @@
 import express from "express";
 import * as dotenv from 'dotenv';
 import cors from 'cors';
-import db from "./Config/db.js"
+import db from "./Config/db.js";
 import { signUp } from "./controllers/register.js";
 import { signIn } from "./controllers/login.js";
 
+// Allowed origins for CORS
+const allowedOrigins = ['http://localhost:3000'];
 
-dotenv.config()
+dotenv.config();
 
-const app =express()
+const app = express();
 
 // Database connection
 db();
 
-//port
-const PORT = process.env.PORT
+// Port
+const PORT = process.env.PORT || 5000; // Fallback to port 5000 if not defined
 
-
-//middleware
-app.use(express.json())
+// Middleware
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// CORS configuration
+app.use(cors({
+  origin: allowedOrigins
+}));
 
 // Routes
 app.get('/', (req, res) => {
-    res.send('Hello World');
+  res.send('Hello World');
 });
 
-//routes
-app.use("/api/user",signUp)
-app.use("/api/signin",signIn)
+// Register route
+app.post("/api/signup", signUp); // Changed to POST
+// SignIn route
+app.post("/api/signin", signIn); // Changed to POST
 
-// Start /server
+// Start server
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
